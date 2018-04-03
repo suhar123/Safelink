bb2485c65f08df5e102dbb2ae0376b81769af8fc
suhar123 <adititik527@gmail.com>
2018-04-04 01:24:06 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -22,7 +22,7 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="what-is-bitcoin.html"}
-if(x=="2"){xxx="powerful-meditation-methods.html "}
+if(x=="2"){xxx="powerful-meditation-methods.html"}
 if(x=="3"){xxx="the-best-tips-for-skin-care.html"}
 if(x=="4"){xxx="what-does-body-need-to-stay-healthy.html"}
 if(x=="5"){xxx="natural-beauty-to-add-shine-to-your-hair.html"}a_to_ve[i].href="https://your-link1.blogspot.co.id/2018/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
